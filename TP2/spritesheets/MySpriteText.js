ba256ade3d89f7b7f17ab2ce1d3d72c35918c6c5
Paulo Ribeiro <up201806505@fe.up.pt>
2020-11-22 15:33:57 +0000

    Aligned primitive MySpriteText

--- TP2/spritesheets/MySpriteText.js
+++ TP2/spritesheets/MySpriteText.js
@@ -9,7 +9,7 @@ class MySpriteText {
         this.scene = scene; 
 
         this.text = text;
-        this.background = new MyRectangle(scene, 0, 0, 1, 1);
+        this.background = new MyRectangle(scene, -0.5, -0.5, 0.5, 0.5);
         this.spriteSheet = new MySpriteSheet(scene, "./scenes/images/spritesheet-alphabet.jpg", 8, 6);
     }
 
@@ -49,7 +49,7 @@ class MySpriteText {
         for (let i = 0; i < this.text.length; i++) {
             // Relative position
             let matrix = mat4.create();
-            mat4.translate(matrix, matrix, [i, 0, 0]);
+            mat4.translate(matrix, matrix, [i - this.text.length/2 + 0.5, 0, 0]);
 
             // Get Char Position and update sprite sheet coordinate
             let MN = this.getPosition(this.text[i]);
@@ -61,6 +61,7 @@ class MySpriteText {
             this.background.display();
             this.scene.popMatrix();
         }
+
         this.scene.setActiveShaderSimple(this.scene.defaultShader);
     }
 
