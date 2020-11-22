66849ed96a7c798a35ca93588d57322550132899
Paulo Ribeiro <up201806505@fe.up.pt>
2020-11-22 12:31:28 +0000

    Documented SpriteSheet, SpriteText and SpriteAnim classes

--- TP2/spritesheets/MySpriteAnim.js
+++ TP2/spritesheets/MySpriteAnim.js
@@ -1,3 +1,12 @@
+/**
+ * MySpriteAnim
+ * @constructor
+ * @param {CGFscene} scene - Reference to MyScene object
+ * @param {MySpriteSheet} spriteSheet - Spritesheet to use in this sprite animation
+ * @param {integer} startCell - First cell of the sprite animation
+ * @param {integer} endCell - Final cell of the sprite animation
+ * @param {integer} duration - Reference to number of stacks of cylinder
+ */
 class MySpriteAnim {
     constructor(scene, spriteSheet, startCell, endCell, duration) {
         this.scene = scene;
@@ -16,33 +25,55 @@ class MySpriteAnim {
         this.elapsedTime = 0;
     }
 
+    /**
+     * Updates the sprite animation, by changing the current cell being presented, based on the elapsed time since the beggining of the sprite animation
+     * @method update
+     * @param {integer} currentTime - Time elapsed since the last call to this method
+     */
     update(currentTime) {
+        // Update elapsed time since the beggining of the sprite animation
         this.elapsedTime += currentTime;
 
-        // Calculate which sprite cell is active
+        // Calculate which sprite cell is active - If the elapsedTime is greater than the time needed to present the current cell, it must change the cell presented
         if (this.elapsedTime >= (this.cellDuration * (this.currentCell + 1))) {
             if (this.currentCell == this.endCell) {     // Reset Animation Loop
                 this.currentCell = this.startCell;
                 this.elapsedTime = 0;
             }
             else
-                this.currentCell++;
+                this.currentCell++;     // Skip to the next cell
         }
     }
 
+    /**
+     * Displays the sprite animation, by displaying its background (MyRectangle), and activating the shader of the sprite animation
+     * @method display
+     */
     display() {
+        // Activate the current cell to be presented
         this.spriteSheet.activateCellP(this.currentCell);
 
+        // Make the background of the sprite animation transparent
         this.scene.gl.enable(this.scene.gl.BLEND);
         this.scene.gl.blendFunc(this.scene.gl.SRC_ALPHA, this.scene.gl.ONE_MINUS_SRC_ALPHA);
         this.scene.gl.depthMask(false);
+
+        // Display the background of the sprite animation
         this.background.display();
 
+        // Remove transparency properties
         this.scene.gl.depthMask(true);
         this.scene.gl.disable(this.scene.gl.BLEND);
+
+        // Activate the shader of the sprite animation
         this.scene.setActiveShader(this.scene.defaultShader);
     }
 
+    /**
+     * Updates the list of texture coordinates
+     * @param {integer} afs - dx/afs
+     * @param {integer} aft - dy/aft
+     */
     updateTexCoords(afs, aft) {
         // Do nothing
     }
--- TP2/spritesheets/MySpriteSheet.js
+++ TP2/spritesheets/MySpriteSheet.js
@@ -1,12 +1,18 @@
+/**
+ * MySpriteSheet
+ * @constructor
+ * @param {CGFscene} scene - Reference to MyScene object
+ * @param {String} texture - Texture to be applied in the sprite sheet
+ * @param {integer} sizeM - Number of Columns of the sprite sheet
+ * @param {integer} sizeN - Number of Lines of the sprite sheet
+ */
 class MySpriteSheet {
     constructor(scene, texture, sizeM, sizeN) {
         this.scene = scene;
 
-        // -- Passed as parameters -- //
         this.sizeM = sizeM;
         this.sizeN = sizeN;
 
-        // -- Texture -- // 
         this.texture = new CGFtexture(this.scene, texture);
 
         this.shader = new CGFshader(this.scene.gl, "./shaders/spriteShader.vert", "./shaders/spriteShader.frag");
@@ -15,17 +21,34 @@ class MySpriteSheet {
         this.shader.setUniformsValues({sizeM: this.sizeM, sizeN: this.sizeN});
     }
 
+    /**
+     * Activates the current cell to be presented, based on the values of the column and row passed by parameters
+     * @method activateCellMN
+     * @param {integer} m - Column of the cell to be presented
+     * @param {integer} n - Line of the cell to be presented
+     */
     activateCellMN(m, n) {
+        // Activate the shader of the sprite sheet
         this.scene.setActiveShaderSimple(this.shader);
         
+        // Pass the values of the line and column of the cell to be presented to the frag and vert files
         this.shader.setUniformsValues({actualM: m, actualN: n});
+
+        // Bind the texture of the spritesheet
         this.texture.bind(0); 
     }
 
+    /**
+     * Activates the current cell to be presented, based on the value corresponding to the number of the cell position passed by parameter
+     * @method activateCellP
+     * @param {integer} p - Number of the cell position to be presented
+     */
     activateCellP(p) {
+        // Calculate Column and Line of the cell to be presented, based on the number of its position
         let M = p % this.sizeM;
         let N = Math.floor(p / this.sizeM);
 
+        // Use method activateCellMN to activate the cell, based on the values calculated before
         this.activateCellMN(M, N);
     }
 }
