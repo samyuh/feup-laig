65539c832015875ef2337743be1675f75b4a267c
Diogo Fernandes <up201806250@fe.up.pt>
2021-01-02 19:44:13 +0000

    Added Pokemons to XML

--- TP3/primitives/obj/CGFOBJModel.js
+++ TP3/primitives/obj/CGFOBJModel.js
@@ -10,8 +10,10 @@
 * and optimized to reduce vertex duplication
 */
 
-class CGFOBJModel extends CGFobject {
-	constructor(scene, url, wireframe) {
+class CGFOBJModel extends CGFobject{
+
+	constructor(scene, url, wireframe) 
+	{
 		super(scene);
 		
 		this.url = url;
@@ -22,7 +24,7 @@ class CGFOBJModel extends CGFobject {
 		// init with empty object, so that there are no problems while the data is being loaded
 		this.vertices = [];
 		this.normals = [];
-		this.texcoords = [];
+		this.texCoords = [];
 		this.indices = [];
 		
 		this.primitiveType = this.scene.gl.TRIANGLES;
@@ -32,12 +34,52 @@ class CGFOBJModel extends CGFobject {
 		this.rr = new CGFresourceReader();
 		this.rr.open(url, this);
 		
-	};
-	onResourceError(string) {
+	}
+	onResourceError(string) 
+	{
 		console.log("Error loading resource " + this.url + ": "+string);
 	}
 
-	onResourceReady(string) {
+	onResourceReady(string) 
+	{
+		function procVert(model, str)
+		{
+			str=str.trim();
+			if (lut[str]) //already stored
+				// just add the index
+				model.indices[icount]=lut[str];
+			else // not stored
+			{
+				// add coords/tex/norms to corresponding buffers and add the index
+				
+				var f = str.split('/');
+				
+				Array.prototype.push.apply(
+					model.vertices, positions[parseInt(f[0]) - 1]
+				);
+				
+				if (f[1])
+					Array.prototype.push.apply(
+						model.texCoords, texcoords[parseInt(f[1]) - 1]
+					);
+				if (f[2])
+					Array.prototype.push.apply(
+						model.normals, normals[parseInt(f[2]) - 1]
+					);
+				
+				// store index in lut
+				
+				lut[str]=model.vcount;
+				
+				model.indices[icount]=model.vcount;
+
+				model.vcount++
+			}
+			icount++;
+		}
+	
+	
+	
 	  var lines = string.split("\n");
 	  var positions = [];
 	  var normals = [];
@@ -54,8 +96,7 @@ class CGFOBJModel extends CGFobject {
 		var parts = lines[i].trimRight().split(/\s+/);
 		if ( parts.length > 0 ) {
 		  switch(parts[0]) {
-			case 'v':  
-			positions.push(
+			case 'v':  positions.push(
 				[
 					parseFloat(parts[1]),
 					parseFloat(parts[2]),
@@ -76,62 +117,33 @@ class CGFOBJModel extends CGFobject {
 			  texcoords.push(
 				[
 				  parseFloat(parts[1]),
-				  parseFloat(parts[2])
+				  1.0-parseFloat(parts[2])	// v coordinate needs to be mirrored
 				]
 			  );
 			  break;
 			case 'f': {
-				function procVert(model, str) {
-					str=str.trim();
-					if (lut[str]) //already stored
-						// just add the index
-						model.indices[icount]=lut[str];
-					else { // not stored 
-						// add coords/tex/norms to corresponding buffers and add the index
-						
-						var f = str.split('/');
-						
-						Array.prototype.push.apply(
-							model.vertices, positions[parseInt(f[0]) - 1]
-						);
-						
-						if (f[1])
-							Array.prototype.push.apply(
-								model.texcoords, texcoords[parseInt(f[1]) - 1]
-							);
-						if (f[2])
-							Array.prototype.push.apply(
-								model.normals, normals[parseInt(f[2]) - 1]
-							);
-						
-						// store index in lut
-						
-						lut[str]=model.vcount;
-						
-						model.indices[icount]=model.vcount;
-
-						model.vcount++
-					}
-					icount++;
-				}
 			
-				if (!this.wireframe) {
+				if (!this.wireframe)
+				{
 					procVert(this,parts[1]);
 					procVert(this,parts[2]);
 					procVert(this,parts[3]);
-					if (parts.length>4) {
+					if (parts.length>4)
+					{
 						procVert(this,parts[1]);
 						procVert(this,parts[3]);
 						procVert(this,parts[4]);
 					}
 				}
-				else {
+				else
+				{
 					procVert(this,parts[1]);
 					procVert(this,parts[2]);
 					procVert(this,parts[2]);
 					procVert(this,parts[3]);
 					procVert(this,parts[3]);
-					if (parts.length > 4) {
+					if (parts.length>4)
+					{
 						procVert(this,parts[4]);
 						procVert(this,parts[4]);
 					}
@@ -147,8 +159,8 @@ class CGFOBJModel extends CGFobject {
 	  }
 	  console.log("Loaded mesh "+ this.url +" with " + this.vcount + " vertices / "+ this.fcount + " faces");
 
-		if (this.texcoords.length==0)
-			this.texcoords=null;
+		if (this.texCoords.length==0)
+			this.texCoords=null;
 			
 		if (!this.wireframe)
 			this.primitiveType = this.scene.gl.TRIANGLES;
@@ -156,5 +168,5 @@ class CGFOBJModel extends CGFobject {
 			this.primitiveType = this.scene.gl.LINES;
 		
 		this.initGLBuffers();
-	};
+	}
 }
--- TP3/primitives/obj/CGFResourceReader.js
+++ TP3/primitives/obj/CGFResourceReader.js
@@ -5,15 +5,15 @@
 * method #onXMLError from a callbackObj, depending whether the read operation was successfull.
 *
 */
-var indexOf = function (needle) {
-	if (typeof Array.prototype.indexOf === 'function') {
+var indexOf = function(needle) {
+	if(typeof Array.prototype.indexOf === 'function') {
 		indexOf = Array.prototype.indexOf;
 	} else {
-		indexOf = function (needle) {
+		indexOf = function(needle) {
 			var i = -1, index = -1;
 
-			for (i = 0; i < this.length; i++) {
-				if (this[i] === needle) {
+			for(i = 0; i < this.length; i++) {
+				if(this[i] === needle) {
 					index = i;
 					break;
 				}
@@ -37,82 +37,88 @@ var indexOf = function (needle) {
  * @constructor
  * Creates a XML Reader with the default values
   */
-class CGFresourceReader {
-	constructor() {
-		this.xmlhttp = null;
-		this.xmlDoc = null;
-		this.resource = null;
-		this.callbackObj = null;
-		this.errorMessage = null;
+CGFresourceReader = function() {
+
+	this.xmlhttp = null;
+	this.xmlDoc = null;
+	this.resource = null;
+	this.callbackObj = null;
+	this.errorMessage = null;
+};
+
+CGFresourceReader.prototype.constructor = CGFresourceReader;
+
+/** @method getErrorMessage
+* Getter of the error message string, if an error happend
+*/
+CGFresourceReader.prototype.getErrorMessage = function() {
+	return this.errorMessage;
+};
+
+/** @method open
+* Opens the url resource from #resource and reads its contents. On success calls the
+* method onResourceReady from the #callbackObj. On error calls the method
+* onResourceError from the #callbackObj.
+* @param {String} resource the url of the resource (xml file) to be read
+* @param {Object} callbackObj an object containing methods onResourceReady() and onResourceError(message)
+*/
+CGFresourceReader.prototype.open = function(resource, callbackObj) 
+{
+	this.resource = resource;
+	if (! callbackObj.onResourceReady)
+		console.error("CGFresourceReader.open: onResourceReady handler not defined.");
+
+	if (! callbackObj.onResourceError)
+		console.error("CGFresourceReader.open: onResourceError handler not defined.");
+	
+	this.callbackObj = callbackObj;
+
+	if (window.XMLHttpRequest) {
+		this.xmlhttp=new XMLHttpRequest();                            // For all modern browsers
+	} 
+	else if (window.ActiveXObject) {
+		this.xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");   		// For (older) IE
 	}
 
-	/** @method getErrorMessage
-	* Getter of the error message string, if an error happend
-	*/
-	getErrorMessage() {
-		return this.errorMessage;
-	};
+	if (this.xmlhttp != null)  {
 
-	/** @method open
-	* Opens the url resource from #resource and reads its contents. On success calls the
-	* method onResourceReady from the #callbackObj. On error calls the method
-	* onResourceError from the #callbackObj.
-	* @param {String} resource the url of the resource (xml file) to be read
-	* @param {Object} callbackObj an object containing methods onResourceReady() and onResourceError(message)
-	*/
-	open(resource, callbackObj) {
-		this.resource = resource;
-		if (!callbackObj.onResourceReady)
-			console.error("CGFresourceReader.open: onResourceReady handler not defined.");
+		this.xmlhttp.onload = this.onStateChange;
+		this.xmlhttp.reader = this;
+		this.xmlhttp.open("GET", resource, true); //  (httpMethod,  URL,  asynchronous)			
+		this.xmlhttp.setRequestHeader("Content-Type", "text/xml");
+		this.xmlhttp.send(null);
+	}
+	else {
+		reader.callbackObj.onResourceError("The XMLHttpRequest is not supported");
+		return;
+	}
+};
 
-		if (!callbackObj.onResourceError)
-			console.error("CGFresourceReader.open: onResourceError handler not defined.");
 
-		this.callbackObj = callbackObj;
+CGFresourceReader.prototype.onStateChange = function() {
 
-		if (window.XMLHttpRequest) {
-			this.xmlhttp = new XMLHttpRequest();                            // For all modern browsers
-		}
-		else if (window.ActiveXObject) {
-			this.xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");   		// For (older) IE
-		}
-
-		if (this.xmlhttp != null) {
-
-			this.xmlhttp.onload = this.onStateChange;
-			this.xmlhttp.reader = this;
-			this.xmlhttp.open("GET", resource, true); //  (httpMethod,  URL,  asynchronous)			
-			this.xmlhttp.setRequestHeader("Content-Type", "text/xml");
-			this.xmlhttp.send(null);
-		}
-		else {
-			reader.callbackObj.onResourceError("The XMLHttpRequest is not supported");
+  if (this.readyState==4) {  // 4 => loaded complete
+	if (this.status==200) {  // HTTP status code  ( 200 => OK )
+		var parser = new window.DOMParser();
+		var reader = this.reader;
+		var response = this.reader.xmlhttp;
+		var text = response.responseText;
+		
+		reader.callbackObj.onResourceReady(text);
+		
+		if (reader.getErrorMessage() != null) {
+			reader.callbackObj.onResourceError(reader.getErrorMessage());
 			return;
 		}
-	};
+		
+	  } 
+	  else {
+		var reader = this.reader;
+		reader.callbackObj.onResourceError(this.status + ": " + reader.resource + ", " + this.statusText);
+	  } 
+   }  
+};
 
-
-	onStateChange() {
-
-		if (this.readyState == 4) {  // 4 => loaded complete
-			if (this.status == 200) {  // HTTP status code  ( 200 => OK )
-				var parser = new window.DOMParser();
-				var reader = this.reader;
-				var response = this.reader.xmlhttp;
-				var text = response.responseText;
-
-				reader.callbackObj.onResourceReady(text);
-
-				if (reader.getErrorMessage() != null) {
-					reader.callbackObj.onResourceError(reader.getErrorMessage());
-					return;
-				}
-
-			}
-			else {
-				var reader = this.reader;
-				reader.callbackObj.onResourceError(this.status + ": " + reader.resource + ", " + this.statusText);
-			}
-		}
-	};
-}
+	
+	
+	
